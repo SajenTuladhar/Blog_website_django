12c8abcd6c122ce1aaf032944a79ac5749064143
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-26 13:38:14 +0545

    url routes and views with css

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def mainpage(request):
+    return render(request,"blog/mainPage.html")
+
+def posts(request):
+    return render(request,"blog/postsPage.html")
+
+def post_details(request):
+    return HttpResponse("the detail of this post is here")
--- Blog_Website/settings.py
+++ Blog_Website/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'Blog_Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/"Templates"
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
