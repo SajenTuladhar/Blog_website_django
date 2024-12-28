adbbef041506867ac94e2ff950ef5c827aa6ad8f
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-29 00:42:45 +0545

    working with database

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Posts,Author,Caption
 
 # Register your models here.
+
+admin.site.register(Posts)
+admin.site.register(Author)
+admin.site.register(Caption)
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name= models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    E_mail=models.EmailField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name},{self.E_mail}"
+    
+
+class Caption(models.Model):
+    text= models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.text
+    
+class Posts(models.Model):
+    title= models.CharField(max_length=100)
+    excerpt= models.CharField(max_length=200)
+    image_name= models.CharField(max_length=100)
+    date= models.DateField(auto_created=True)
+    slug=models.SlugField()
+    content=models.TextField(max_length=500)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f"{self.title} ,{self.date}"
